42f9462097853d6c38e58d5b5f893dafd2c21966
cencek <87336335+cencek@users.noreply.github.com>
2022-12-03 18:06:07 -0500

    Update demo.R

--- demo.R
+++ demo.R
@@ -4,4 +4,56 @@
 readRenviron("~/Documents/Credentials/.Renviron")
 
 library(paws)
-ec2 <- paws::ec2()
+
+# PDF Location
+
+bucket <- "mis685-textract-demo"
+file <- "textract-demo.pdf"
+
+textract <- paws::textract()
+
+# Import all associated PDF data through Textract
+# --------------------------------------------------
+analyze_document <- function(bucket, file) {
+  
+  # Start analyzing the PDF.
+  resp <- textract$start_document_analysis(
+    DocumentLocation = list(
+      S3Object = list(Bucket = bucket, Name = file)
+    ),
+    FeatureTypes = "TABLES"
+  )
+  
+  # Check that the analysis is done and get the result.
+  count <- 0
+  while (count < 30 && (!exists("result") || result$JobStatus == "IN_PROGRESS")) {
+    Sys.sleep(1)
+    result <- textract$get_document_analysis(
+      JobId = resp$JobId
+    )
+    # If the result has multiple parts, get the remaining parts.
+    next_token <- result$NextToken
+    while (length(next_token) > 0) {
+      next_result <- textract$get_document_analysis(
+        JobId = resp$JobId,
+        NextToken = next_token
+      )
+      result$Blocks <- c(result$Blocks, next_result$Blocks)
+      next_token <- next_result$NextToken
+    }
+    count <- count + 1
+  }
+  
+  return(result)
+}
+
+analysis <- analyze_document(bucket, file)
+
+#-------------------------------------------------------------------------------
+
+
+results <- unlist(analysis, recursive=FALSE)
+
+
+flattenedResults <- unlist(analysis)
+View(flattenedResults)
